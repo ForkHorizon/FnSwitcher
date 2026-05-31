e27775ae26a8d783f09bd608fb1c6313a495918c
Daliys <daliys133@gmail.com>
2026-05-31 13:46:11 +0200

    init

---
+++ AppleLocalizationSwitcher/AppleLocalizationSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLocalizationSwitcherApp.swift
+//  AppleLocalizationSwitcher
+//
+//  Created by Kiryl Shcherba on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AppleLocalizationSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
